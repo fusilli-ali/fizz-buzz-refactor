5ec258b89eb3231f6e1cbce58f3e1fc0024eeedd
Alexandra B <abaldari@smarsh.com>
2016-03-30 16:30:39 -0700

    idk what i'm doing

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+for (var counter = 1; counter <= 100; counter = counter + 1) 
+{
+    if ((counter % 3)==0 && (counter % 5)==0){
+    	$("#fizz-buzz").append("<li>fizz buzz</li>");
+    }
+    else {
+    	if ((counter % 3)==0){
+    		$("#fizz-buzz").append("<li>fizz</li>");
+    	}
+    	else {
+			if ((counter % 5)==0){
+    			$("#fizz-buzz").append("<li>buzz</li>");
+    		}
+    		else {
+    			$("#fizz-buzz").append("<li>"+counter+"</li>");
+    		}
+    	}
+
+    }
+}
+});

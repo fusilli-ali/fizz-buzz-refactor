357fc7eac8163197bfa8e63334144e76effab0d4
Alexandra B <abaldari@smarsh.com>
2016-04-06 13:51:33 -0700

    maybe?

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var number = window.prompt("Gimme a number!", "Any number!");
 for (var counter = 1; counter <= 100; counter = counter + 1) 
 {
     if ((counter % 3)==0 && (counter % 5)==0){
@@ -20,3 +21,28 @@ for (var counter = 1; counter <= 100; counter = counter + 1)
     }
 }
 });
+
+
+/*$(document).ready(function(){
+for (var counter = 1; counter <= 100; counter = counter + 1) 
+{
+    if ((counter % 3)==0 && (counter % 5)==0){
+        $("#fizz-buzz").append("<li>fizz buzz</li>");
+    }
+    else {
+        if ((counter % 3)==0){
+            $("#fizz-buzz").append("<li>fizz</li>");
+        }
+        else {
+            if ((counter % 5)==0){
+                $("#fizz-buzz").append("<li>buzz</li>");
+            }
+            else {
+                $("#fizz-buzz").append("<li>"+counter+"</li>");
+            }
+        }
+
+    }
+}
+});
+*/
